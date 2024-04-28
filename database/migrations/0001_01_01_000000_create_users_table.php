fb3472fa512103996bc10bc5ec920010a689f6bb
Abdullah Alhaj <torgodly@gmail.com>
2024-04-28 11:47:13 +0200

    add active and phoen to user table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -16,8 +16,10 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('phone')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('type')->default(UserType::User);
+            $table->boolean('active')->default(true);
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
